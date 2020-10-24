f928ec578b35a91fe566cd0c066178d9d5ade3b5
dbamaster <crobles@dbamastery.com>
2020-10-24 16:29:03 -0600

    New application and database schema version (2.0)

--- WebApp/Models/Countries.cs
+++ WebApp/Models/Countries.cs
@@ -1,14 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace geo_kids_web_net.Models
 {
+    [Table("vw_countries")]
     public class Countries
     {
         [Key]
         public string country_id { get; set; }
         public string country { get; set; }
-        public string un_m49 {get; set;}
-        public int region_id {get; set;}
+        public string un_m49 {get; set; }
+        public int region_id {get; set; }
+        public string flag { get; set; }
     }
 }
